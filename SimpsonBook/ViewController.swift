bd4e39cce1fc3379534d0832cdc7f60a4e022b3b
Kubraakk <kbr3460@icloud.com>
2024-03-12 11:37:39 +0300

    Initial Commit

---
+++ SimpsonBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpsonBook
+//
+//  Created by Kübra Akpunar (Teknoloji Mimarisi Bölümü) on 12.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
